89d5505dc720380344f5c371aefab4cc61a245ea
Kimen Tang <kimentanm@gmail.com>
2022-01-14 17:46:34 +0800

    refactor: reformat code

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import os
 import threading
 from urllib import request
 
+
 class Iptv(object):
     playlist_file = 'playlists/'
     m3u8_file_path = 'output/'
@@ -23,6 +24,7 @@ class Iptv(object):
         self.T = utils.tools.Tools()
         self.now = int(time.time() * 1000)
 
+    # 从网络url中获取源文件
     def getPlaylistFromNetwork(self, url):
         playList = []
         if url[-4:] == '.txt':
@@ -55,6 +57,7 @@ class Iptv(object):
                 print(e)
         return playList
 
+    # 从本地获取直播源文件
     def getPlaylist(self):
 
         '''
@@ -66,25 +69,25 @@ class Iptv(object):
         战旗柯南1,http://dlhls.cdn.zhanqi.tv/zqlive/69410_SgVxl.m3u8
         战旗柯南2,http://alhls.cdn.zhanqi.tv/zqlive/219628_O3y9l.m3u8
         '''
-        playList=[]
-        #读取文件
+        playList = []
+        # 读取文件
         path = os.listdir(self.playlist_file)
         for p in path:
             if os.path.isfile(self.playlist_file + p):
-                if p[-4:]=='.txt':
-                    with open(self.playlist_file + p,'r') as f:
+                if p[-4:] == '.txt':
+                    with open(self.playlist_file + p, 'r') as f:
                         lines = f.readlines()
                         total = len(lines)
                         for i in range(0, total):
                             line = lines[i].strip('\n')
                             item = line.split(',', 1)
-                            if len(item)==2:
+                            if len(item) == 2:
                                 data = {
                                     'title': item[0],
                                     'url': item[1],
                                 }
                                 playList.append(data)
-                elif p[-4:]=='.m3u':
+                elif p[-4:] == '.m3u':
 
                     try:
                         m3u8_obj = m3u8.load(self.playlist_file + p)
@@ -92,7 +95,7 @@ class Iptv(object):
                         for i in range(0, total):
                             tmp_title = m3u8_obj.segments[i].title
                             tmp_url = m3u8_obj.segments[i].uri
-                            data={
+                            data = {
                                 'title': tmp_title,
                                 'url': tmp_url,
                             }
@@ -106,10 +109,10 @@ class Iptv(object):
         :return: True or False
         验证每一个直播源；对于有效直播源才插入数据库；同时，如果数据库中存在title相同的记录，则比较DELAY时间，保存DELAY小的。
         '''
-        total=len(playList)
-        if (total<=0): return False
+        total = len(playList)
+        if (total <= 0): return False
         threadList = []
-        for i in range(0,total):
+        for i in range(0, total):
             print('Checking[ %s / %s ]:%s' % (i, total, playList[i]['title']))
             thread_check = threading.Thread(target=self.checkPlay, args=(playList[i],))
             thread_check.start()
@@ -135,11 +138,10 @@ class Iptv(object):
         else:
             pass
 
-
-    def addData (self, data) :
+    def addData(self, data):
         title = data['title']
         try:
-            if title not in self.resultData :
+            if title not in self.resultData:
                 self.resultData[title] = data
             else:
                 old_delay = self.resultData[data['title']]['delay']
@@ -158,22 +160,23 @@ class Iptv(object):
             # 排序
             sorted(result, key=lambda value: value['title'])
 
-            if len(result)>0 :
-                output_file=self.m3u8_file_path+ str(time.time()*1000) +'.m3u'
-                with open (output_file,'w') as f:
+            if len(result) > 0:
+                output_file = self.m3u8_file_path + str(time.time() * 1000) + '.m3u'
+                with open(output_file, 'w') as f:
                     f.write("#EXTM3U\n")
                     for item in result:
                         f.write("#EXTINF:-1, %s\n" % (item['title']))
                         f.write("%s\n" % (item['url']))
-                print('共获得 %s 个有效直播源！' %(len(result)))
+                print('共获得 %s 个有效直播源！' % (len(result)))
             else:
                 print('无有效直播源！')
                 pass
         except Exception as e:
             print(e)
 
+
 if __name__ == '__main__':
-    iptv=Iptv()
+    iptv = Iptv()
     print('开始......')
     playlist = iptv.getPlaylistFromNetwork('https://gitee.com/happiness-2022/mytv/raw/master/mytv2.txt')
     # playlist = iptv.getPlaylist()
